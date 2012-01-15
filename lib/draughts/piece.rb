4d0153ee127eea48e2647a33331575d48c40b0f8
Pablo A. Torres Navarrete <tn.pablo@gmail.com>
2012-01-15 16:27:13 -0500

    Black pieces and white pieces get their own class

--- lib/draughts/piece.rb
+++ lib/draughts/piece.rb
@@ -1,9 +1,10 @@
+require 'draughts/utils'
+
 module Draughts
   class Piece
     attr_reader :color
 
-    def initialize(color)
-      @color = color
+    def initialize
       @king = false
     end
 
@@ -17,6 +18,40 @@ module Draughts
 
     def to_s
       @color.to_s[0].capitalize
+    end
+  end
+
+  class BlackPiece < Piece
+    def initialize
+      super
+      @color = :black
+    end
+
+    def valid_move?(from, to)
+      row = Utils.index_to_row(from)
+
+      if row.even?
+        [from + 3, from + 4].include? to
+      else
+        [from + 4, from + 5].include? to
+      end
+    end
+  end
+
+  class WhitePiece < Piece
+    def initialize
+      super
+      @color = :white
+    end
+
+    def valid_move?(from, to)
+      row = Utils.index_to_row(from)
+
+      if row.even?
+        [from + -4, from + -5].include? to
+      else
+        [from + -3, from + -4].include? to
+      end
     end
   end
 end
