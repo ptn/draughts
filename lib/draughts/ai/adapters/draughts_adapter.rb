48f1de2e40cbf74dfea063f16f67bcac5e695b89
Pablo A. Torres Navarrete <tn.pablo@gmail.com>
2012-01-19 18:06:42 -0500

    Last file shuffling

---
+++ lib/draughts/ai/adapters/draughts_adapter.rb
@@ -0,0 +1,15 @@
+module Draughts
+  module AI
+    module Adapters
+      #
+      # A class that talks to the Draughts engine.
+      #
+      # This adapter knows the formats that the Draughts engine requires for
+      # input and output. It sends the bots' moves and parses the next board
+      # that they have to play.
+      #
+      class DraughtsAdapter
+      end
+    end
+  end
+end
