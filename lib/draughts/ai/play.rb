eb42437a857a44231ecec610968fd0851502d5af
Pablo Torres <tn.pablo@gmail.com>
2012-01-27 23:02:30 -0500

    Don't make board+move a primary key on Play

--- lib/draughts/ai/play.rb
+++ lib/draughts/ai/play.rb
@@ -3,13 +3,14 @@ module Draughts
     class Play
       include DataMapper::Resource
 
+      property :id,    Serial
       property :legal, Boolean
       property :color, String
 
       validates_within :color, :set => ["black", "white"]
 
-      belongs_to :board, :key => true
-      belongs_to :move,  :key => true
+      belongs_to :board
+      belongs_to :move
     end
   end
 end
