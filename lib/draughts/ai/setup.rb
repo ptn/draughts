51af8c6ca58b60c1a7ebb4724ed2867a355a919a
Pablo Torres <tn.pablo@gmail.com>
2012-01-29 18:55:50 -0500

    Don't require ~/.draughts to exist

--- lib/draughts/ai/setup.rb
+++ lib/draughts/ai/setup.rb
@@ -1,8 +1,9 @@
 require 'data_mapper'
+require_relative '../../../config/bots'
 
 module Draughts
   module AI
-    base = File.expand_path("~/.draughts")
+    base = File.expand_path(Config::DBDIR)
     DataMapper::Logger.new(File.join(base, 'db.log'), :debug)
     DataMapper.setup(:default, 'sqlite://' + File.join(base,'draughts.db'))
   end
@@ -11,7 +12,6 @@ end
 require_relative 'board'
 require_relative 'move'
 require_relative 'play'
-require_relative '../../../config/bots'
 
 DataMapper.finalize
 DataMapper.auto_upgrade!
