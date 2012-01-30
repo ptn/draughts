5ee57eb614fc32d19eafd4643cee1e1579847855
Pablo Torres <tn.pablo@gmail.com>
2012-01-30 01:28:29 -0500

    Don't log queries by default

--- lib/draughts/ai/setup.rb
+++ lib/draughts/ai/setup.rb
@@ -2,8 +2,13 @@ require 'data_mapper'
 
 module Draughts
   module AI
-    base = File.expand_path(Config::DBDIR)
-    DataMapper::Logger.new(File.join(base, 'db.log'), :debug)
+    base = File.expand_path(Config::DB_DIR)
+
+    if Config::DB_DEBUG
+      logfile = File.join(base, 'db.log')
+      DataMapper::Logger.new(logfile, :debug)
+    end
+
     DataMapper.setup(:default, 'sqlite://' + File.join(base,'draughts.db'))
   end
 end
